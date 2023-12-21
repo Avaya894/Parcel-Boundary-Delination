9472fda4492a532816710676bda75fb7a75a2f11
Avaya894 <bajracharyaavaya52@gmail.com>
2023-12-21 23:31:13 +0545

    Second version

--- boundary_Parcel_Demo3.py
+++ boundary_Parcel_Demo3.py
@@ -250,30 +250,47 @@ def on_hover(event):
 def on_leave(event):
     event.widget.config(style="TButton")
 
+def set_select_file():
+    filename = askopenfilename()
+    e1.insert(0, filename)
+
+def on_hover(event):
+    event.widget.config(style="Hover.TButton")
+
+def on_leave(event):
+    event.widget.config(style="TButton")
+
+'''
+# Create a new style for the GUI
+style = ttk.Style()
+style.theme_create("custom_style", parent="clam", settings={
+    "TButton": {
+        "configure": {"background": "#3498db", "foreground": "white", "font": ('Arial', 12)},
+        "map": {"background": [("active", "#2980b9")]}
+    },
+    "TEntry": {
+        "configure": {"font": ('Arial', 12)}
+    },
+    "TLabel": {
+        "configure": {"font": ('Arial', 12)}
+    }
+})
+style.theme_use("custom_style")
+
 root = Tk()
-root.style = ttk.Style()
 
+W = str(root.winfo_screenwidth())
+H = str(root.winfo_screenheight())
+root.configure(width=W, height=H, bg="#2ecc71")  # Background color changed to a green shade
+root.title("Modified Boundary Parcel Delination")
 
-root.style.theme_use("clam")
-
-width = root.winfo_screenwidth()
-height = root.winfo_screenheight()
-root.style.configure('TEntry', padding=(10, 5, 10, 5), font=('Arial', 12))
-root.style.configure('TButton', padding=(10, 5, 10, 5), font=('Arial', 12))
-
-root.style.map('Hover.TButton',
-          foreground=[('active', 'red')],
-          background=[('active', 'green')])
-
-W = str(width)
-H = str(height)
-root.configure(width=W, height=H, bg="red")
-root.title("Boundary Parcel Delination")
 v = StringVar()
-left_frame = Frame(root, width=400, height=1000, bg='green')
+
+left_frame = Frame(root, width=400, height=1000, bg='#3498db')  # Background color changed to a blue shade
 left_frame.grid(row=0, column=0, padx=10, pady=5)
-right_frame = Frame(root, width=750, height=500, bg='lightgrey')
-right_frame.grid(row=0, column=1, padx=10, pady=5, sticky="nsew", )
+
+right_frame = Frame(root, width=750, height=500, bg='#ecf0f1')  # Background color changed to a light gray shade
+right_frame.grid(row=0, column=1, padx=10, pady=5, sticky="nsew")
 
 fig = Figure(figsize=(6, 6))
 canvas = FigureCanvasTkAgg(fig, master=right_frame)
@@ -283,32 +300,34 @@ toolbar = Frame(left_frame, width=180, height=185)
 tool_bar.grid(row=2, column=0, padx=5, pady=5)
 toolbar.grid(row=10, column=0, padx=5, pady=20)
 
-label_filename = ttk.Label(tool_bar, text="     Select shape file(.shp)")
+label_filename = ttk.Label(tool_bar, text="Select shape file(.shp)")
 label_filename.grid(row=0, column=0, padx=5, pady=3, ipadx=10)
+
 e1 = ttk.Entry(tool_bar, textvariable=v)
 e1.grid(row=1, column=0)
 
-button_browse = ttk.Button(tool_bar, text="Open", command=set_select_file)
+button_browse = ttk.Button(tool_bar, text="Browse", command=set_select_file, style='TButton')
 button_browse.grid(row=2, column=0, padx=5, pady=5)
 button_browse.bind("<Enter>", on_hover)
 button_browse.bind("<Leave>", on_leave)
 
-label_parcel_number = ttk.Label(tool_bar, text="     Parcel Number ")
+label_parcel_number = ttk.Label(tool_bar, text="Parcel Number")
 label_parcel_number.grid(row=3, column=0, padx=5, pady=3, ipadx=10)
+
 e2 = ttk.Entry(tool_bar)
 e2.grid(row=4, column=0)
 
-button_show = ttk.Button(tool_bar, text="Show", command=show_result, style='TButton')
+button_show = ttk.Button(tool_bar, text="Show", style='TButton')
 button_show.grid(row=5, column=0, pady=4, ipadx=10)
 button_show.bind("<Enter>", on_hover)
 button_show.bind("<Leave>", on_leave)
 
-button_quit = ttk.Button(tool_bar, text="Quit", command=root.quit)
+button_quit = ttk.Button(tool_bar, text="Quit", command=root.quit, style='TButton')
 button_quit.grid(row=6, column=0, pady=4, ipadx=10)
 button_quit.bind("<Enter>", on_hover)
 button_quit.bind("<Leave>", on_leave)
 
-button_display_parcel = ttk.Button(toolbar, text="Show Parcel Direction", command=get_ewns_parcel)
+button_display_parcel = ttk.Button(toolbar, text="Show Parcel Direction", command=get_ewns_parcel, style='TButton')
 button_display_parcel.grid(row=6, column=0, pady=4, ipadx=10)
 button_display_parcel.bind("<Enter>", on_hover)
 button_display_parcel.bind("<Leave>", on_leave)
@@ -317,3 +336,176 @@ label_output = ttk.Label(toolbar, text="Output")
 label_output.grid(row=0, column=0, padx=5, pady=5)
 
 root.mainloop()
+'''
+
+import tkinter as tk
+import tkinter.messagebox
+import customtkinter
+
+customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
+customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
+
+
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+
+        # configure window
+        self.title("CustomTkinter complex_example.py")
+        self.geometry(f"{1100}x{580}")
+
+        # configure grid layout (4x4)
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_columnconfigure((2, 3), weight=0)
+        self.grid_rowconfigure((0, 1, 2), weight=1)
+
+          
+        # create sidebar frame with widgets
+        self.sidebar_frame = customtkinter.CTkFrame(self, width=140, corner_radius=0)
+        self.sidebar_frame.grid(row=0, column=0, rowspan=4, sticky="nsew")
+        self.sidebar_frame.grid_rowconfigure(5, weight=1)
+        self.logo_label = customtkinter.CTkLabel(self.sidebar_frame, text="Boundary Detx", font=customtkinter.CTkFont(size=20, weight="bold"))
+        self.logo_label.grid(row=0, column=0, padx=20, pady=(20, 10))
+        self.sidebar_button_1 = customtkinter.CTkButton(self.sidebar_frame, command=self.sidebar_button_event, text="Browse File")
+        self.sidebar_button_1.grid(row=1, column=0, padx=20, pady=10)
+        self.sidebar_button_2 = customtkinter.CTkButton(self.sidebar_frame, command=self.open_input_dialog_event, text="Enter Parcel Number")
+        self.sidebar_button_2.grid(row=2, column=0, padx=20, pady=10)
+        self.sidebar_button_3 = customtkinter.CTkButton(self.sidebar_frame, command=self.sidebar_button_event, text="Show Parcel")
+        self.sidebar_button_3.grid(row=3, column=0, padx=20, pady=10)
+        self.sidebar_button_4 = customtkinter.CTkButton(self.sidebar_frame, command=self.sidebar_button_event, text="Quit")
+        self.sidebar_button_4.grid(row=4, column=0, padx=20, pady=10)
+        self.sidebar_button_5 = customtkinter.CTkButton(self.sidebar_frame, command=self.sidebar_button_event, text="Show parcel direction")
+        self.sidebar_button_5.grid(row=5, column=0, padx=20, pady=10)
+        
+
+
+        self.appearance_mode_label = customtkinter.CTkLabel(self.sidebar_frame, text="Appearance Mode:", anchor="w")
+        self.appearance_mode_label.grid(row=7, column=0, padx=20, pady=(10, 0))
+        self.appearance_mode_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["Light", "Dark", "System"],
+                                                                       command=self.change_appearance_mode_event)
+        self.appearance_mode_optionemenu.grid(row=8, column=0, padx=20, pady=(10, 10))
+        self.scaling_label = customtkinter.CTkLabel(self.sidebar_frame, text="UI Scaling:", anchor="w")
+        self.scaling_label.grid(row=9, column=0, padx=20, pady=(10, 0))
+        self.scaling_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["80%", "90%", "100%", "110%", "120%"],
+                                                               command=self.change_scaling_event)
+        self.scaling_optionemenu.grid(row=10, column=0, padx=20, pady=(10, 20))
+
+        # create main entry and button
+        self.entry = customtkinter.CTkEntry(self, placeholder_text="CTkEntry")
+        self.entry.grid(row=3, column=1, columnspan=2, padx=(20, 0), pady=(20, 20), sticky="nsew")
+
+        self.main_button_1 = customtkinter.CTkButton(master=self, fg_color="transparent", border_width=2, text_color=("gray10", "#DCE4EE"))
+        self.main_button_1.grid(row=3, column=3, padx=(20, 20), pady=(20, 20), sticky="nsew")
+
+        # create textbox
+        self.textbox = customtkinter.CTkTextbox(self, width=250)
+        self.textbox.grid(row=0, column=1, padx=(20, 0), pady=(20, 0), sticky="nsew")
+
+        # create tabview
+        # self.tabview = customtkinter.CTkTabview(self, width=250)
+        # self.tabview.grid(row=0, column=2, padx=(20, 0), pady=(20, 0), sticky="nsew")
+        # self.tabview.add("CTkTabview")
+        # self.tabview.add("Tab 2")
+        # self.tabview.add("Tab 3")
+        # self.tabview.tab("CTkTabview").grid_columnconfigure(0, weight=1)  # configure grid of individual tabs
+        # self.tabview.tab("Tab 2").grid_columnconfigure(0, weight=1)
+
+        # self.optionmenu_1 = customtkinter.CTkOptionMenu(self.tabview.tab("CTkTabview"), dynamic_resizing=False,
+        #                                                 values=["Value 1", "Value 2", "Value Long Long Long"])
+        # self.optionmenu_1.grid(row=0, column=0, padx=20, pady=(20, 10))
+        # self.combobox_1 = customtkinter.CTkComboBox(self.tabview.tab("CTkTabview"),
+        #                                             values=["Value 1", "Value 2", "Value Long....."])
+        # self.combobox_1.grid(row=1, column=0, padx=20, pady=(10, 10))
+        # self.string_input_button = customtkinter.CTkButton(self.tabview.tab("CTkTabview"), text="Open CTkInputDialog",
+        #                                                    command=self.open_input_dialog_event)
+        # self.string_input_button.grid(row=2, column=0, padx=20, pady=(10, 10))
+        # self.label_tab_2 = customtkinter.CTkLabel(self.tabview.tab("Tab 2"), text="CTkLabel on Tab 2")
+        # self.label_tab_2.grid(row=0, column=0, padx=20, pady=20)
+
+        # create radiobutton frame
+        # self.radiobutton_frame = customtkinter.CTkFrame(self)
+        # self.radiobutton_frame.grid(row=0, column=3, padx=(20, 20), pady=(20, 0), sticky="nsew")
+        # self.radio_var = tkinter.IntVar(value=0)
+        # self.label_radio_group = customtkinter.CTkLabel(master=self.radiobutton_frame, text="CTkRadioButton Group:")
+        # self.label_radio_group.grid(row=0, column=2, columnspan=1, padx=10, pady=10, sticky="")
+        # self.radio_button_1 = customtkinter.CTkRadioButton(master=self.radiobutton_frame, variable=self.radio_var, value=0)
+        # self.radio_button_1.grid(row=1, column=2, pady=10, padx=20, sticky="n")
+        # self.radio_button_2 = customtkinter.CTkRadioButton(master=self.radiobutton_frame, variable=self.radio_var, value=1)
+        # self.radio_button_2.grid(row=2, column=2, pady=10, padx=20, sticky="n")
+        # self.radio_button_3 = customtkinter.CTkRadioButton(master=self.radiobutton_frame, variable=self.radio_var, value=2)
+        # self.radio_button_3.grid(row=3, column=2, pady=10, padx=20, sticky="n")
+
+        # create slider and progressbar frame
+        # self.slider_progressbar_frame = customtkinter.CTkFrame(self, fg_color="transparent")
+        # self.slider_progressbar_frame.grid(row=1, column=1, padx=(20, 0), pady=(20, 0), sticky="nsew")
+        # self.slider_progressbar_frame.grid_columnconfigure(0, weight=1)
+        # self.slider_progressbar_frame.grid_rowconfigure(4, weight=1)
+        # self.seg_button_1 = customtkinter.CTkSegmentedButton(self.slider_progressbar_frame)
+        # self.seg_button_1.grid(row=0, column=0, padx=(20, 10), pady=(10, 10), sticky="ew")
+        # self.progressbar_1 = customtkinter.CTkProgressBar(self.slider_progressbar_frame)
+        # self.progressbar_1.grid(row=1, column=0, padx=(20, 10), pady=(10, 10), sticky="ew")
+        # self.progressbar_2 = customtkinter.CTkProgressBar(self.slider_progressbar_frame)
+        # self.progressbar_2.grid(row=2, column=0, padx=(20, 10), pady=(10, 10), sticky="ew")
+        # self.slider_1 = customtkinter.CTkSlider(self.slider_progressbar_frame, from_=0, to=1, number_of_steps=4)
+        # self.slider_1.grid(row=3, column=0, padx=(20, 10), pady=(10, 10), sticky="ew")
+        # self.slider_2 = customtkinter.CTkSlider(self.slider_progressbar_frame, orientation="vertical")
+        # self.slider_2.grid(row=0, column=1, rowspan=5, padx=(10, 10), pady=(10, 10), sticky="ns")
+        # self.progressbar_3 = customtkinter.CTkProgressBar(self.slider_progressbar_frame, orientation="vertical")
+        # self.progressbar_3.grid(row=0, column=2, rowspan=5, padx=(10, 20), pady=(10, 10), sticky="ns")
+
+        # create scrollable frame
+        # self.scrollable_frame = customtkinter.CTkScrollableFrame(self, label_text="CTkScrollableFrame")
+        # self.scrollable_frame.grid(row=1, column=2, padx=(20, 0), pady=(20, 0), sticky="nsew")
+        # self.scrollable_frame.grid_columnconfigure(0, weight=1)
+        # self.scrollable_frame_switches = []
+        # for i in range(100):
+        #     switch = customtkinter.CTkSwitch(master=self.scrollable_frame, text=f"CTkSwitch {i}")
+        #     switch.grid(row=i, column=0, padx=10, pady=(0, 20))
+        #     self.scrollable_frame_switches.append(switch)
+
+        # # create checkbox and switch frame
+        # self.checkbox_slider_frame = customtkinter.CTkFrame(self)
+        # self.checkbox_slider_frame.grid(row=1, column=3, padx=(20, 20), pady=(20, 0), sticky="nsew")
+        # self.checkbox_1 = customtkinter.CTkCheckBox(master=self.checkbox_slider_frame)
+        # self.checkbox_1.grid(row=1, column=0, pady=(20, 0), padx=20, sticky="n")
+        # self.checkbox_2 = customtkinter.CTkCheckBox(master=self.checkbox_slider_frame)
+        # self.checkbox_2.grid(row=2, column=0, pady=(20, 0), padx=20, sticky="n")
+        # self.checkbox_3 = customtkinter.CTkCheckBox(master=self.checkbox_slider_frame)
+        # self.checkbox_3.grid(row=3, column=0, pady=20, padx=20, sticky="n")
+
+        # set default values
+        # self.checkbox_3.configure(state="disabled")
+        # self.checkbox_1.select()
+        # self.scrollable_frame_switches[0].select()
+        # self.scrollable_frame_switches[4].select()
+        # self.radio_button_3.configure(state="disabled")
+        self.appearance_mode_optionemenu.set("Dark")
+        self.scaling_optionemenu.set("100%")
+        # self.optionmenu_1.set("CTkOptionmenu")
+        # self.combobox_1.set("CTkComboBox")
+        # self.slider_1.configure(command=self.progressbar_2.set)
+        # self.slider_2.configure(command=self.progressbar_3.set)
+        # self.progressbar_1.configure(mode="indeterminnate")
+        # self.progressbar_1.start()
+        self.textbox.insert("0.0", "CTkTextbox\n\n" + "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.\n\n" * 20)
+        # self.seg_button_1.configure(values=["CTkSegmentedButton", "Value 2", "Value 3"])
+        # self.seg_button_1.set("Value 2")
+
+    def open_input_dialog_event(self):
+        dialog = customtkinter.CTkInputDialog(text="Type in a number:", title="CTkInputDialog")
+        print("CTkInputDialog:", dialog.get_input())
+
+    def change_appearance_mode_event(self, new_appearance_mode: str):
+        customtkinter.set_appearance_mode(new_appearance_mode)
+
+    def change_scaling_event(self, new_scaling: str):
+        new_scaling_float = int(new_scaling.replace("%", "")) / 100
+        customtkinter.set_widget_scaling(new_scaling_float)
+
+    def sidebar_button_event(self):
+        print("sidebar_button click")
+
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
